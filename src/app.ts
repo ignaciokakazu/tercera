2603a0ed4db3babdb88d91c79a71b5e94f849d72
Ignacio Kakazu <ignaciokakazu@gmail.com>
2021-11-18 02:35:02 -0300

    sec

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,6 @@ import {apiLogin} from './apis/login';
 
 const port = process.env.PORT || config.PORT;
 
-
-// apiLogin.get('ignaciokakazu1@gmail.com')
-//     .then((result) => {console.log(result)})
-
-// apiLogin.getByEmail('ignaciokakazu1@gmail.com')
-//     .then((result) => {console.log(result)})
-
 myServer.listen(port, ()=> { infoLogger.info(`SERVER UP ${port}`)});   
 myServer.on('error', (err:any)=> { peligroLogger.warn(`Error en server ${err.message}`)})
 
